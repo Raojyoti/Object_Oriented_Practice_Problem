10c55903b9c569ed12f52f06a874b2d72eca6395
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-12 18:00:39 +0530

    Print the Cards received by the 4 Players

---
+++ Deck_Of _Cards/DeckOfCards.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Deck_Of__Cards
+{
+    public class DeckOfCards
+    {
+
+        public const int ncard = 52;
+        public Card[] deckOfCards;
+        public int currentCard;
+        public Random randNum;
+        public  DeckOfCards()
+        {
+            deckOfCards = new Card[ncard];
+            int i = 0;
+            for (int suit = 1; suit <= 4; suit++)
+                for (int rank = 1; rank <= 13; rank++)
+                    deckOfCards[i++] = new Card(suit, rank);
+                     currentCard = 0;
+        }
+
+        public void Shuffle(int n)
+        {
+            int i, j;
+            randNum = new Random();
+            for (int k = 0; k < n; k++)
+            {
+                i = (int)(randNum.Next(ncard));
+                j = (int)(randNum.Next(ncard));
+                Card tmp = deckOfCards[i];
+                deckOfCards[i] = deckOfCards[j];
+                deckOfCards[j] = tmp;
+            }
+
+            currentCard = 0;
+        }
+
+        public Card Deal()
+        {
+            if (currentCard < ncard)
+                return (deckOfCards[currentCard++]);
+            else
+                return (null);
+        }
+    }
+    public class Card
+    {
+        public static string[] Rank = { "*", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+        public static string[] Suit = { "*", "Clubs", "Diamonds", "Hearts", "Spades" };
+        public byte cardSuit;
+        public byte cardRank;
+        public Card(int suit, int rank)
+        {
+            if (rank == 1)
+                cardRank = 14;
+            else
+                cardRank = (byte)rank;
+            cardSuit = (byte)suit;
+        }
+
+        public int Suits()
+        {
+            return (cardSuit);
+        }
+
+        public int Ranks()
+        {
+            return (cardRank);
+        }
+
+        public String toString()
+        {
+            return (Rank[cardRank] + Suit[cardSuit]);
+        }
+    }
+}
+
---
+++ Deck_Of _Cards/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Deck_Of__Cards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DeckOfCards deck = new DeckOfCards();
+            deck.Shuffle(100);
+            List<string>[] player = new List<string>[4];
+            for (int h = 0; h < 4; h++)
+            {
+                player[h] = new List<string>();
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    player[j].Add(deck.Deal().toString());
+                }
+            }
+            for (int k = 0; k < 4; k++)
+            {
+                List<string> sortedList = player[k].OrderBy(x => PadNumbers(x)).OrderBy(g => FaceValue(g)).ToList();
+                Console.WriteLine("Player=> " + (k + 1) + ": " + string.Join("   ", sortedList));
+                Console.WriteLine("\n");
+            }
+        }
+        public static string PadNumbers(string input)
+        {
+            return Regex.Replace(input, "[0-9]+", match => match.Value.PadLeft(2, '0'));
+        }
+        public static int FaceValue(string input)
+        {
+            return input.Substring(0, 1) == "Ace" ? 14 : input.Substring(0, 1) == "King" ? 13 : input.Substring(0, 1) == "Queen" ? 12 : input.Substring(0, 1) == "Jack" ? 11 : 0;
+        }
+    }
+}
